b9ca274ecac03dc3c33a7e5c394856179852e12e
adull <adlaiabdelrazaq@gmail.com>
2018-12-26 11:12:16 +0800

    removed sign up

--- src/components/account/AccountNotLoggedIn.js
+++ src/components/account/AccountNotLoggedIn.js
@@ -6,11 +6,11 @@ class AccountNotLoggedIn extends Component {
     return(
       <div className="account-no-user">
         <div className="account-top">
-          <div className="cta text-blue-arial">Log in or sign up!</div>
+          <div className="cta text-blue-arial">Log in to smart smiling!</div>
+          <div className="cta-small text-blue-arial-small">No new users are being accepted at this time, feel free to scroll through and enjoy the smiles that we have had!</div>
         </div>
         <div className="account-bottom">
           <AccountButton value="login" loginSuccess = {this.props.login}/>
-          <AccountButton value="signup" signupSuccess = {this.props.login}/>
         </div>
       </div>
     )
